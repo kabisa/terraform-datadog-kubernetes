88bb6ab9e8e16f18a0e1b99120c512a859ea33b5
Mark Maas <mark@maas-martin.nl>
2021-06-14 13:19:40 +0200

    Adding a check for failed PVC's

---
+++ persistent-volumes-variables.tf
@@ -0,0 +1,66 @@
+variable "persistent_volumes_enabled" {
+  type    = bool
+  default = true
+}
+
+variable "persistent_volumes_warning" {
+  type    = number
+  default = 0
+}
+
+variable "persistent_volumes_critical" {
+  type    = number
+  default = 1
+}
+
+variable "persistent_volumes_evaluation_period" {
+  type    = string
+  default = "last_5m"
+}
+
+variable "persistent_volumes_severity" {
+  type    = string
+  default = "minor"
+}
+
+variable "persistent_volumes_note" {
+  type    = string
+  default = ""
+}
+
+variable "persistent_volumes_docs" {
+  type    = string
+  default = ""
+}
+
+variable "persistent_volumes_filter_override" {
+  type    = string
+  default = ""
+}
+
+variable "persistent_volumes_alerting_enabled" {
+  type    = bool
+  default = true
+}
+
+variable "persistent_volumes_no_data_timeframe" {
+  type    = number
+  default = null
+}
+
+variable "persistent_volumes_notify_no_data" {
+  type    = bool
+  default = false
+}
+
+variable "persistent_volumes_ok_threshold" {
+  type    = number
+  default = null
+}
+
+variable "persistent_volumes_priority" {
+  description = "Number from 1 (high) to 5 (low)."
+
+  type    = number
+  default = null
+}
---
+++ persistent-volumes.tf
@@ -0,0 +1,34 @@
+locals {
+  persistent_volumes_filter = coalesce(
+    var.persistent_volumes_filter_override,
+    var.filter_str
+  )
+}
+
+module "persistent_volumes_low" {
+  source = "git@github.com:kabisa/terraform-datadog-generic-monitor.git?ref=0.6.0"
+
+  name             = "Failed Persistent Volume Claims"
+  query            = "avg(${var.persistent_volumes_evaluation_period}):max:kubernetes_state.persistentvolumes.by_phase{${local.persistent_volumes_filter} AND phase:failed} > ${var.persistent_volumes_critical}"
+  alert_message    = "There are failed Physical Volume Claims, storage has problems"
+  recovery_message = "There are no failed Physical Volume Claims"
+
+  # monitor level vars
+  enabled            = var.persistent_volumes_enabled
+  alerting_enabled   = var.persistent_volumes_alerting_enabled
+  critical_threshold = var.persistent_volumes_critical
+  warning_threshold  = var.persistent_volumes_warning
+  priority           = var.persistent_volumes_priority
+  severity           = var.persistent_volumes_severity
+  docs               = var.persistent_volumes_docs
+  note               = var.persistent_volumes_note
+
+  # module level vars
+  env                  = var.alert_env
+  service              = var.service
+  notification_channel = var.notification_channel
+  additional_tags      = var.additional_tags
+  locked               = var.locked
+  name_prefix          = var.name_prefix
+  name_suffix          = var.name_suffix
+}
