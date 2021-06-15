455cc0f8c045b2a628b9dbb3e01fae5b49127411
Mark Maas <mark@maas-martin.nl>
2021-06-15 11:44:50 +0200

    Adding cpu and memory checks that where missing

---
+++ cpu-on-dns-pods-high-variables.tf
@@ -0,0 +1,66 @@
+variable "cpu_on_dns_pods_high_enabled" {
+  type    = bool
+  default = true
+}
+
+variable "cpu_on_dns_pods_high_warning" {
+  type    = number
+  default = 70
+}
+
+variable "cpu_on_dns_pods_high_critical" {
+  type    = number
+  default = 85
+}
+
+variable "cpu_on_dns_pods_high_evaluation_period" {
+  type    = string
+  default = "last_5m"
+}
+
+variable "cpu_on_dns_pods_high_severity" {
+  type    = string
+  default = "major"
+}
+
+variable "cpu_on_dns_pods_high_note" {
+  type    = string
+  default = ""
+}
+
+variable "cpu_on_dns_pods_high_docs" {
+  type    = string
+  default = ""
+}
+
+variable "cpu_on_dns_pods_high_filter_override" {
+  type    = string
+  default = "kube_service:kube-dns OR short_image:coredns OR short_image:ucp-coredns OR short_image:ucp-kube-dns"
+}
+
+variable "cpu_on_dns_pods_high_alerting_enabled" {
+  type    = bool
+  default = true
+}
+
+variable "cpu_on_dns_pods_high_no_data_timeframe" {
+  type    = number
+  default = null
+}
+
+variable "cpu_on_dns_pods_high_notify_no_data" {
+  type    = bool
+  default = false
+}
+
+variable "cpu_on_dns_pods_high_ok_threshold" {
+  type    = number
+  default = null
+}
+
+variable "cpu_on_dns_pods_high_priority" {
+  description = "Number from 1 (high) to 5 (low)."
+
+  type    = number
+  default = null
+}
---
+++ cpu-requests-low.tf
@@ -0,0 +1,34 @@
+locals {
+  cpu_requests_low_filter = coalesce(
+    var.cpu_requests_low_filter_override,
+    var.filter_str
+  )
+}
+
+module "cpu_requests_low" {
+  source = "git@github.com:kabisa/terraform-datadog-generic-monitor.git?ref=0.6.0"
+
+  name             = "Available CPU for Requests Low"
+  query            = "max(${var.cpu_requests_low_evaluation_period}):sum:kubernetes.cpu.capacity{${local.cpu_requests_low_filter}} by {host,cluster_name} - sum:kubernetes.cpu.requests{${local.cpu_requests_low_filter}} by {host,cluster_name} < ${var.cpu_requests_low_critical}"
+  alert_message    = "Kubernetes cluster cpu room for requests is too low"
+  recovery_message = "Kubernetes cluster cpu requests is ok again."
+
+  # monitor level vars
+  enabled            = var.cpu_requests_low_enabled
+  alerting_enabled   = var.cpu_requests_low_alerting_enabled
+  critical_threshold = var.cpu_requests_low_critical
+  warning_threshold  = var.cpu_requests_low_warning
+  priority           = var.cpu_requests_low_priority
+  severity           = var.cpu_requests_low_severity
+  docs               = var.cpu_requests_low_docs
+  note               = var.cpu_requests_low_note
+
+  # module level vars
+  env                  = var.alert_env
+  service              = var.service
+  notification_channel = var.notification_channel
+  additional_tags      = var.additional_tags
+  locked               = var.locked
+  name_prefix          = var.name_prefix
+  name_suffix          = var.name_suffix
+}
