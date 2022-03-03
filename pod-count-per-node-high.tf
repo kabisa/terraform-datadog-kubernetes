b904f02876097560b8548f5893bffe014587c769
Sjuul Janssen <sjuul@outlook.com>
2022-03-03 09:45:33 +0100

    fix bug

--- pod-count-per-node-high.tf
+++ pod-count-per-node-high.tf
@@ -17,14 +17,14 @@ module "pod_count_per_node_high" {
   recovery_message    = "Pod count per node high ({{ value }}) in {{ service }} has recovered"
 
   # monitor level vars
-  enabled                     = var.pod_count_per_node_high_enabled
-  alerting_enabled            = var.pod_count_per_node_high_alerting_enabled
-  warning_threshold           = var.pod_count_per_node_high_warning
-  warning_recovery_threshold  = var.pod_count_per_node_high_warning_recovery
-  critical_recovery_threshold = var.pod_count_per_node_high_critical_recovery
-  priority                    = var.pod_count_per_node_high_priority
-  docs                        = var.pod_count_per_node_high_docs
-  note                        = var.pod_count_per_node_high_note
+  enabled           = var.pod_count_per_node_high_enabled
+  alerting_enabled  = var.pod_count_per_node_high_alerting_enabled
+  warning_threshold = var.pod_count_per_node_high_warning
+  warning_recovery  = var.pod_count_per_node_high_warning_recovery
+  critical_recovery = var.pod_count_per_node_high_critical_recovery
+  priority          = var.pod_count_per_node_high_priority
+  docs              = var.pod_count_per_node_high_docs
+  note              = var.pod_count_per_node_high_note
 
   # module level vars
   env                  = var.env
