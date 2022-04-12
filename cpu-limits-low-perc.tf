f77629a8d72cabba50b1a4bb15de104fe862f539
Sjuul Janssen <sjuul@outlook.com>
2022-04-12 13:25:38 +0200

    had requests and limits mixed up in one monitor

--- cpu-limits-low-perc.tf
+++ cpu-limits-low-perc.tf
@@ -10,7 +10,7 @@ module "cpu_limits_low_perc" {
   version = "0.7.1"
 
   name             = "Available CPU for Limits in percentages Low"
-  query            = "max(${var.cpu_limits_low_perc_evaluation_period}):(sum:kubernetes.cpu.requests{${local.cpu_limits_low_perc_filter}} by {host,cluster_name} / max:system.cpu.num_cores{${local.cpu_limits_low_perc_filter}} by {host,cluster_name}) * 100 > ${var.cpu_limits_low_perc_critical}"
+  query            = "max(${var.cpu_limits_low_perc_evaluation_period}):(sum:kubernetes.cpu.limits{${local.cpu_limits_low_perc_filter}} by {host,cluster_name} / max:system.cpu.num_cores{${local.cpu_limits_low_perc_filter}} by {host,cluster_name}) * 100 > ${var.cpu_limits_low_perc_critical}"
   alert_message    = "Kubernetes cluster cpu room for limits / percentage is too low"
   recovery_message = "Kubernetes cluster cpu limits / percentage has recovered"
 
