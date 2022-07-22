a260a08504f44a09cb3f2377977f04296a204bd4
Sjuul Janssen <sjuul@outlook.com>
2022-07-22 08:52:41 +0200

    removed alert_env from example as thats deprecated

--- examples/example.tf
+++ examples/example.tf
@@ -5,6 +5,5 @@ module "kubernetes" {
   notification_channel = "mail@example.com"
   service              = "Kubernetes"
   env                  = "prd"
-  alert_env            = "prd"
   filter_str           = "cluster_name:production"
 }
